c2fe137cbfc42f73961c1b0460fd1a2b0bab7512
Farel Ferdyawan <ferdy.transafe@gmail.com>
2026-02-09 16:39:17 +0700

    add searching n pagination

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -9,7 +9,7 @@
             @foreach ($categories as $category)
                 
             <div class="col-md-4">
-                <a href="/categories/{{ $category->slug }}">
+                <a href="/posts?category={{ $category->slug }}">
                 <div class="card text-bg-dark text-white">
                     <img src="https://picsum.photos/500/500?{{ $category->name }}" 
                     class="card-img" alt="{{ $category->name }}">
