883839af113a0e4b3476e5d0d95fe7fe02a76a8e
Farel Ferdyawan <ferdy.transafe@gmail.com>
2026-02-10 16:30:31 +0700

    add userLogin n middleware

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -12,28 +12,54 @@
       </div>
       @endif
 
+      @if (session()->has('loginError'))
+      <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        {{ session('loginError') }}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>
+      @endif
+
       <main class="form-signin w-100 m-auto">
         <h1 class="h3 mb-3 fw-normal">Please Login</h1>
 
-    <form>
+    <form action="/login" method="POST">
+      @csrf
       <div class="form-floating">
         <input
           type="email"
-          class="form-control"
-          id="floatingInput"
+          name="email"
+          class="form-control @error('email') is-invalid @enderror"
+          id="email"
           placeholder="name@example.com"
+          required
+          autofocus
+          value="{{ old('email') }}"
         />
-        <label for="floatingInput">Email address</label>
+        <label for="email">Email address</label>
       </div>
+      @error('email')
+        <div class="invalid-feedback">
+          {{ $message }}
+        </div>
+      @enderror
+
       <div class="form-floating">
         <input
           type="password"
+          name="password"
           class="form-control"
-          id="floatingPassword"
+          id="password"
           placeholder="Password"
+          required
         />
-        <label for="floatingPassword">Password</label>
+        <label for="password">Password</label>
+        @error('password')
+          <div class="invalid-feedback">
+            {{ $message }}
+          </div>
+        @enderror
       </div>
+
       <button class="btn btn-primary w-100 py-2" type="submit">
         Login
       </button>
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -21,10 +21,26 @@
       </ul>
 
       <ul class="navbar-nav ms-auto">
+        @auth
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Welcome Back, {{ auth()->user()->name }}
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="/dashboard"><i class="bi bi-layout-text-window"></i> My Dashboard</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <form action="/logout" method="POST">
+              @csrf
+              <button type="submit" class="dropdown-item" href="#"><i class="bi bi-box-arrow-right"></i> Logout</a></li></button>
+            </form>
+          </ul>
+        </li>
+        @else
         <li class="nav-item">
           <a href="/login" class="nav-link {{ $active === "login" ? 'active' : "" }}"><i class="bi bi-box-arrow-in-right"></i>
             Login</a>
         </li>
+        @endauth
       </ul>
     </div>
   </div>
